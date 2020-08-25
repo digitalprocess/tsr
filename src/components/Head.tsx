79946bded84fcac7394fcb2042c00fff772cd30f
frontendista <csdelreal@gmail.com>
2020-08-25 09:46:36 -0500

    Default & Optional Props

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -2,14 +2,15 @@ import React from 'react'
 
 type Props = {
 	title: string,
-	isActive: boolean
+	isActive?: boolean
 }
 
-const Head = ({ title, isActive }: Props) => (
+const Head = ({ title, isActive = false }: Props) => (
 	<div>
 		<h1>{title}</h1>
 		{isActive &&
-			<h2>Active</h2>}
+			<h2>Active</h2>
+		}
 	</div>
 )
 

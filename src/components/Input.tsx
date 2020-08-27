064ceacf0ff53baf793decab3cd93284b70503b6
frontendista <csdelreal@gmail.com>
2020-08-26 19:15:03 -0500

    useRef and Typing Dom Elements

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,11 +1,18 @@
-import React, {useState} from 'react'
+import React, { useState, useRef } from 'react'
 
 const Input = () => {
 	// const [name, setName] = useState<string>(''); // Explicit Type
 	const [name, setName] = useState('') // Type inference
+	const ref = useRef<HTMLInputElement>(null!)
+
+	if (ref && ref.current) {
+		console.log(ref.current.value)
+	}
+
 	return (
 		<div>
 			<input
+				ref={ref}
 				type="text"
 				value={name}
 				onChange={ev => setName(ev.target.value)}
